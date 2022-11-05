a94db920f4885e263268a11feb5f8e737bc1f87f
Juan Tuñon <primerproyecto@gmail.com>
2022-11-05 23:20:28 +0100

    ejercicio5. Funcionando para una unica pregunta

--- 5.yoda/exercise-5.js
+++ 5.yoda/exercise-5.js
@@ -1,3 +1,6 @@
+
+const OPENTDB_TOKEN = 'e297e082aa44bfe4c1067dd3e84634da711dacdee9134a67369ff18e7cb83337'
+
 // Funciones
 const empezarJuego = (e) => {
     const $input = document.querySelector('[data-function="questions-number"]');
@@ -8,6 +11,7 @@ const empezarJuego = (e) => {
     fetch('https://opentdb.com/api.php?amount=' + $input.value)
         .then(response => response.json())
         .then(data => {
+            console.log(data)
             const preguntas = data.results;
             for(const pregunta of preguntas){
                 const { category, correct_answer, difficulty, question, type } = pregunta
@@ -17,31 +21,36 @@ const empezarJuego = (e) => {
                     <fieldset>
                         <legend><strong>${question}</strong></legend>
                         <div class="labelWrapper">
-                             ${todasLasRespuesta.map(item => `<label><input name="${correct_answer}" type="radio" class="radioButton" />${item}</label>`).join('')} 
+                             ${todasLasRespuesta.map((item, i) => `<label><input name="manuel" type="radio" class="radioButton" data-tuna="${todasLasRespuesta[i]}" />${item}</label>`).join('')} 
                         </div>
                     </fieldset>
                 `;
-                
-                $gameboard.innerHTML += templateHtmlItem
+                $gameboard.innerHTML += templateHtmlItem;
             }
+
+
+            //funcion checkear
+            const $form = document.querySelector('#gameForm');
+            const chekearFormulario = (e) => {
+                e.preventDefault();
+                // selecciono todos los fieldsets
+                const $selecionados = document.querySelectorAll('input[name="manuel"]');
+                for(const seleciona of $selecionados){
+                    seleciona.parentElement.style.border = 'none'
+                }
+                const $selecionado = document.querySelector('input[name="manuel"]:checked');
+
+                if ($selecionado.dataset.tuna == data.results[0].correct_answer) {
+                    $selecionado.parentElement.style.border = '2px solid green'
+                } else {
+                    $selecionado.parentElement.style.border = '2px solid red'
+                }
+            }
+            $form.addEventListener('submit', chekearFormulario);
         });
     }
 // función que lanza el ebotón check game
-const chekearFormulario = (e) => {
-    e.preventDefault();
-    // selecciono todos los fieldsets
-    const $fieldsets = document.querySelectorAll('fieldset')
-    $fieldsets.forEach(fieldset => {
-        // selecciono todos los inputs de los fieldsets y los meto en array
-        const $inputs = Array.from(fieldset.querySelectorAll('input'));
-        //
-        if($inputs[($inputs.length -1)].checked) {
-            console.log('acertaste')
-        } else {
-            console.log('vuelve a intentarlo')
-        }        
-    })
-}
+
 
 //boton empezar juego
 const $startGameButton = document.querySelector('[data-function="start-game"]');
@@ -56,14 +65,18 @@ const $input = document.querySelector('[data-function="questions-number"]');
 const $checkButton = document.querySelector('[data-function="check-game"]');
 
 //seleciono del formulario
-const $form = document.querySelector('#gameForm');
+
 
 
 // agrego evento submit al formulario
-$form.addEventListener('submit', chekearFormulario);
+
 $startGameButton.addEventListener('click', empezarJuego);
 
 
 
+const $resetButton = document.querySelector('#reset');
 
-
+$resetButton.addEventListener('click', function(e){
+    e.preventDefault();
+    $gameboard.innerHTML = ''
+})
